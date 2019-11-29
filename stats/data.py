a0db96c5e3451535c2523d343e143db4b344b742
TravisChristensen <travis.christensen@target.com>
2019-11-29 03:29:52 -0600

    fixed tests

--- stats/data.py
+++ stats/data.py
@@ -2,12 +2,10 @@ import os
 import glob
 import pandas as pd
 
-game_files_path = os.path.join(os.getcwd(), 'games', '*.EVE')
-game_files = glob.glob(game_files_path)
+game_files = glob.glob(os.path.join(os.getcwd(), 'games', '*.EVE'))
 game_files.sort()
 
-game_frames = list()
-
+game_frames = []
 for game_file in game_files:
     game_frame = pd.read_csv(game_file, names=['type', 'multi2', 'multi3', 'multi4', 'multi5', 'multi6', 'event'])
     game_frames.append(game_frame)
@@ -19,7 +17,8 @@ identifiers = games['multi2'].str.extract(r'(.LS(\d{4})\d{5})')
 identifiers = identifiers.fillna(method='ffill')
 identifiers.columns = ['game_id', 'year']
 
-games = pd.concat([games, identifiers], axis=1, sort=False).fillna(value=' ')
+games = pd.concat([games, identifiers], axis=1, sort=False)
+games = games.fillna(' ')
 games.loc[:, 'type'] = pd.Categorical(games.loc[:, 'type'])
 
-games.head()
+print(games.head())
